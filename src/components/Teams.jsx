aced8300eddc65f9819067ba892bc0adbda3c9e1
orionwilx <wilman3d@hotmail.com>
2023-11-21 19:40:30 -0500

    detallitos css casi nada

--- src/components/Teams.jsx
+++ src/components/Teams.jsx
@@ -2,20 +2,32 @@ import React from 'react';
 import ResponsiveAppBar from './navmui';
 import './Teams.css';
 
-const Team = ({ image, name, score }) => (
+const Team = ({ image, name, dicT }) => (
 	<div className='team'>
 		<img className='logo' src={image} alt={name} />
 		<h2 className='name'> {name}</h2>
-		<p className='score'>{score}</p>
+		<p className='score'>{dicT}</p>
 	</div>
 );
 
 const Home = () => {
 	const teams = [
-		{ image: 'https://flagcdn.com/lr.svg', name: 'USA', dicT: 2 },
-		{ image: 'https://flagcdn.com/co.svg', name: 'COLOMBIA', dicT: 1 },
-		{ image: 'https://flagcdn.com/co.svg', name: 'COLOMBIA', dicT: 1 },
-		{ image: 'https://flagcdn.com/co.svg', name: 'COLOMBIA', dicT: 1 },
+		{ image: 'https://flagcdn.com/lr.svg', name: 'USA', dicT: 'Newman' },
+		{
+			image: 'https://flagcdn.com/co.svg',
+			name: 'COLOMBIA',
+			dicT: 'perekerman',
+		},
+		{
+			image: 'https://flagcdn.com/co.svg',
+			name: 'COLOMBIA',
+			dicT: 'perekerman',
+		},
+		{
+			image: 'https://flagcdn.com/co.svg',
+			name: 'COLOMBIA',
+			dicT: 'perekerman',
+		},
 		// ...otros equipos
 	];
 
@@ -27,10 +39,12 @@ const Home = () => {
 			<section className='App-header'>
 				<h1>Equipos</h1>
 			</section>
-			<main>
-				{teams.map((team, index) => (
-					<Team key={index} {...team} />
-				))}
+			<main className='App-main'>
+				<div className='container-main'>
+					{teams.map((team, index) => (
+						<Team key={index} {...team} />
+					))}
+				</div>
 			</main>
 		</div>
 	);
